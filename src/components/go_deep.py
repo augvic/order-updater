5a4752d8dcd243a8d44886ffba44d223f15d0a94
AugustoViceli <a.viceli7357@gmail.com>
2025-10-20 19:17:33 -0300

    Fix in file output name.

--- src/components/go_deep.py
+++ src/components/go_deep.py
@@ -16,7 +16,7 @@ class GoDeep:
         self.session.post("https://positivo-pme.f1b2b.com.br/admin", data=self.payload)
         
     def export_orders_to_csv(self) -> None:
-        destiny = path.abspath(path.join(path.dirname(__file__), "..", "..", "storage", "orders.csv"))
+        destiny = path.abspath(path.join(path.dirname(__file__), "..", "..", "storage", ".orders.csv"))
         response = self.session.get("https://positivo-pme.f1b2b.com.br/admin/orders/export-csv")
         with open(destiny, "wb") as file:
             file.write(response.content)
