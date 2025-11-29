9509c908a7bbb9f353b1e3e2f94778dfe8a48a3d
AugustoViceli <a.viceli7357@gmail.com>
2025-11-29 10:16:00 -0300

    Created loop.

--- src/io/cli.py
+++ src/io/cli.py
@@ -1,4 +1,5 @@
 from src.tasks.update_orders import UpdateOrders
+import time
 
 class Cli:
     
@@ -7,9 +8,9 @@ class Cli:
             update_orders_task = UpdateOrders()
             while True:
                 update_orders_task.execute()
-                response = input('❗ Para reiniciar aperte ENTER. Para sair digite "SAIR": ')
-                if response == "SAIR":
-                    break
+                print("")
+                print('⏳ Aguardando 30m para reinício.')
+                time.sleep(1800)
                 print("")
         except Exception as error:
             print(error)
